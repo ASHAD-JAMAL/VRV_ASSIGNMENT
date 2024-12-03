ce57b48dbb419c22aa5f2bab938c1c9f664926ab
Ashad Jamal <ashadjamalhyt@gmail.com>
2024-12-03 14:14:22 +0530

    remove topology

--- api/config/db.js
+++ api/config/db.js
@@ -1,16 +1,13 @@
 const mongoose = require("mongoose");
 
-const dbConnection = (url) => {
-  mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  const db = mongoose.connection;
-  db.on("error", (error) => {
-    console.log("Error connecting to database", error);
-  });
-  db.once("open", () => {
+const dbConnection = async (url) => {
+  try {
+    await mongoose.connect(url);
     console.log("Connected to database");
-  });
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+    process.exit(1);
+  }
 };
+
 module.exports = dbConnection;
